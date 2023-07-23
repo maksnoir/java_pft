3905a41943d2832e315a840a7b44ecd70b0544be
maksnoir <maksnoir@gmail.com>
2023-07-23 22:32:55 +0400

    HW3 refactoring

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -10,7 +10,6 @@ public class PointTest {
         Point p1 = new Point(2, 3);
         Point p2 = new Point(4, 5);
         Assert.assertEquals(p1.distanceTo(p2),2.8284271247461903);
-        Assert.assertNotEquals(p1.distanceTo(p2),0);
     }
     @Test
     public void testDistance2() {
