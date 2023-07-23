4a0c74b40c494d02b4581694a6880e27465ba8d9
maksnoir <maksnoir@gmail.com>
2023-07-23 21:24:27 +0400

    HW 2

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(1, 1);
+        Point p2 = new Point(2, 2);
+        System.out.println("1-3 Расстояние между двумя точками " + p1 + " и " + p2 + " = " + distance(p1, p2));
+        System.out.println("4 Расстояние между двумя точками " + p1.distance(p2));
+    }
+
+    public static double distance(Point p1, Point p2){
+        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+    }
+}
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,42 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double distance(Point p2){
+        return Math.sqrt(Math.pow(p2.getX() - getX(), 2) + Math.pow(p2.getY() - getY(), 2));
+    }
+
+    @Override
+    public String toString() {
+        return "Point{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}
+
+
+
